f72c4dbb34df72ec1aa6b62be9a3b44cda8c1145
vishaaal <officialvk02@gmail.com>
2023-02-19 10:53:41 +0530

    navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,28 +1,54 @@
-import React from 'react'
-import codeLogo from '../../Assets/codeLogo.png'
-import search from '../../Assets/search.png'
-import translate from '../../Assets/translate.png'
-import moon from '../../Assets/moon.png'
-
-
+import React from "react";
+import codeLogo from "../../Assets/codeLogo.png";
+import search from "../../Assets/search.png";
+import translate from "../../Assets/translate.png";
+import moon from "../../Assets/moon.png";
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light d-inline-flex p-2 w-100">
-      <div className='px-2'><img src={codeLogo} /></div>
-      
-      <div className='px-2'><img src={search}/></div>
+    <div>
+      <div className="navbar navbar-expand-lg navbar-light bg-light d-inline-flex p-4 w-100" >
+      <div className="d-inline-flex">
+        <div className="px-2">
+          <img src={codeLogo} />
+        </div>
+      </div>
+      <div className="d-inline-flex" style={{position:"fixed", right:"0"}}>
+        <div className="px-4">
+          <img src={search} />
+        </div>
 
-      <div className='px-2'><img src={moon}/></div>
+        <div className="px-4">
+          <img src={moon} />
+        </div>
 
-      <div className='px-2'><button type="button" class="btn btn-primary">Start Preparing Now</button></div>
+        <div className="px-4">
+          <button type="button" class="btn btn-primary">
+            Start Preparing Now
+          </button>
+        </div>
+      </div>
+    </div>
+    <div className="navbar d-inline-flex p-2 w-100 justify-content-center " style={{backgroundColor:" #EFF6FF"}}>
+        <div className="px-5">
+          Blogs
+        </div>
+        <div className="px-5 justifyContentEnd">
+          Interview Prep
+        </div>
 
-      
-      
-      
-      
+        <div className="px-5">
+          Interview Experience
+        </div>
+        <div className="px-5">
+          Write for Us
+        </div>
+        <div className="px-5">
+          About Us
+        </div>
 
-  
-</nav>
-  )
+    </div>
+    </div>
+    
+  );
 }
