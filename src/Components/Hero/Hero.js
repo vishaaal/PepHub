341b812985c8c8691bc4731af0f3c94a0c3c7527
vishaaal <officialvk02@gmail.com>
2023-02-19 11:59:41 +0530

    hero section

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import peplogo from "../../Assets/logo.svg";
+import herosec from "../../Assets/heroSectionImg.svg"
 
 export default function hero() {
   return (
-    <div>hero</div>
-  )
+    <div className="d-flex justify-content-center flex-column align-items-center  mt-5" >
+      <div className="pb-2">
+        <img src={peplogo} style={{ height: "50px", width: "auto" }} />
+      </div>
+
+      <div className="pb-4">
+        your prerparation for that SDE Role <br />
+        Better and easier than ever
+      </div>
+      <div>
+      <img src={herosec} style={{ height: "400px", width: "auto" }} />
+      </div>
+    </div>
+  );
 }
